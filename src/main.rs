a73e26f2e4bf3bc0105c4833edb1976a88906a6c
Johan R-G <usercise@users.noreply.github.com>
2026-09-16 19:44:57 +0100

    feat: add a render window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+//! An empty application (does nothing)
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Running Bevy App");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Close the window to return to the main function".into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Update, system)
+        .run();
+    println!("Bevy App has exited. We are back in our main function.");
+}
+
+fn system() {
+    info!("Logging from Bevy App");
 }
